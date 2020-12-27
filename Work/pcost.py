552def70f900cf0f13aa7bb255eea6a27f72e285
Tero Kukkola <tero.kukkola@gmail.com>
2020-12-27 21:25:09 +0200

    add csv module to function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,18 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27 /1.30 function / 1.32 csv module
-       
+import csv      
 def portfolio_cost(filename):
     'This function will calculate total cost given the input file'
     total_price = 0
     with open(filename, 'rt') as f:
-        headers = next(f).split(',')    
-        for line in f:
-            row = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        print(headers)    
+        for row in rows:
+            #row = line.split(',')
             try:
                 total_price = total_price + int(row[1]) * float(row[2])
             except ValueError:
-                print("Could't parse", line)
+                print("Could't parse", row)
         return(total_price)        
 
 cost = portfolio_cost('data/portfolio.csv')
