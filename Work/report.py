9625d3199f9114f31c2e1fa0763e43598a68a9e5
Tero Kukkola <tero.kukkola@gmail.com>
2020-12-31 08:25:46 +0200

    add read_portfolio function with tuple logic

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,14 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+def read_portfolio(filename):
+    'This function will open a given portfolio file and convert it to list of tuples'
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
